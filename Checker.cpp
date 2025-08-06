27a213a387ae7663fb1cda0e495d7b05ca54ce8e
AJITHkm50 <fjt2kor@bosch.com>
2025-08-06 10:34:31 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,16 +2,33 @@
 #include <iostream>
 using namespace std;
 
-bool allParamsAreOk(float temperature, float soc, float chargeRate) {
-  return (temperature >= 0 && temperature <= 45)
-      && (soc >= 20 && soc <= 80)
-      && (chargeRate <= 0.8);
+// Function to check temperature condition
+bool isTemperatureOk(float temperature) {
+    return (temperature >= 0 && temperature <= 45);
 }
+
+// Function to check state of charge (SOC) condition
+bool isSocOk(float soc) {
+    return (soc >= 20 && soc <= 80);
+}
+
+// Function to check charge rate condition
+bool isChargeRateOk(float chargeRate) {
+    return (chargeRate <= 0.8);
+}
+
+// Main function to check all parameters
+bool allParamsAreOk(float temperature, float soc, float chargeRate) {
+    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
+}
+
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  return allParamsAreOk(temperature, soc, chargeRate);
+    return allParamsAreOk(temperature, soc, chargeRate);
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(30, 50, 0.9) == false);
+    assert(batteryIsOk(-5, 50, 0.5) == false);
 }
